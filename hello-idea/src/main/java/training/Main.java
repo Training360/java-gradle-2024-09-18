b889acc86c7a20663f18945a32f0151afcf4ac55
vicziani <viczian.istvan@gmail.com>
2024-09-18 15:03:38 +0200

    QR kód generálás

--- hello-idea/src/main/java/training/Main.java
+++ hello-idea/src/main/java/training/Main.java
@@ -1,17 +1,28 @@
 package training;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        new Main().writeCodeToFile("John Doe", Path.of("barcode.png"));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public void writeCodeToFile(String text, Path path) {
+        BitMatrix matrix = null;
+        try {
+            matrix = new MultiFormatWriter()
+                    .encode(text, BarcodeFormat.QR_CODE, 200, 200);
+            MatrixToImageWriter.writeToPath(matrix, "png", path);
+        } catch (WriterException | IOException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
